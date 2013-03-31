ad07f1ec947d9bba6c4c6e713f0278df60096f7d
Michael Zick Doherty <mzdoherty@gmail.com>
2013-03-30 23:31:48 -0400

    swapped lux lib

--- libraries/Adafruit_TSL2561/Adafruit_TSL2561.h
+++ libraries/Adafruit_TSL2561/Adafruit_TSL2561.h
@@ -1,14 +1,13 @@
-
 /**************************************************************************/
 /*! 
-    @file     tsl2561.h
-    @author   K. Townsend (microBuilder.eu)
+    @file     Adafruit_TSL2561.h
+    @author   K. Townsend (Adafruit Industries)
 
     @section LICENSE
 
     Software License Agreement (BSD License)
 
-    Copyright (c) 2010, microBuilder SARL
+    Copyright (c) 2013, Adafruit Industries
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -34,7 +33,6 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 /**************************************************************************/
-
 #ifndef _TSL2561_H_
 #define _TSL2561_H_
 
@@ -43,23 +41,21 @@
 #else
  #include <WProgram.h>
 #endif
-// personal linking errors with arduino1.0 ?
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 #define TSL2561_VISIBLE 2                   // channel 0 - channel 1
 #define TSL2561_INFRARED 1                  // channel 1
 #define TSL2561_FULLSPECTRUM 0              // channel 0
 
-// 3 i2c address options!
-#define TSL2561_ADDR_LOW  0x29
-#define TSL2561_ADDR_FLOAT 0x39
-#define TSL2561_ADDR_HIGH 0x49
+// I2C address options
+#define TSL2561_ADDR_LOW          (0x29)
+#define TSL2561_ADDR_FLOAT        (0x39)    // Default address (pin left floating)
+#define TSL2561_ADDR_HIGH         (0x49)
 
 // Lux calculations differ slightly for CS package
 //#define TSL2561_PACKAGE_CS
 #define TSL2561_PACKAGE_T_FN_CL
-
-#define TSL2561_READBIT           (0x01)
 
 #define TSL2561_COMMAND_BIT       (0x80)    // Must be 1
 #define TSL2561_CLEAR_BIT         (0x40)    // Clears any pending interrupt (write 1 to clear)
@@ -127,6 +123,19 @@
 #define TSL2561_LUX_B8C           (0x0000)  // 0.000 * 2^LUX_SCALE
 #define TSL2561_LUX_M8C           (0x0000)  // 0.000 * 2^LUX_SCALE
 
+// Auto-gain thresholds
+#define TSL2561_AGC_THI_13MS      (4850)    // Max value at Ti 13ms = 5047
+#define TSL2561_AGC_TLO_13MS      (100)
+#define TSL2561_AGC_THI_101MS     (36000)   // Max value at Ti 101ms = 37177
+#define TSL2561_AGC_TLO_101MS     (200)
+#define TSL2561_AGC_THI_402MS     (63000)   // Max value at Ti 402ms = 65535
+#define TSL2561_AGC_TLO_402MS     (500)
+
+// Clipping thresholds
+#define TSL2561_CLIPPING_13MS     (4900)
+#define TSL2561_CLIPPING_101MS    (37000)
+#define TSL2561_CLIPPING_402MS    (65000)
+
 enum
 {
   TSL2561_REGISTER_CONTROL          = 0x00,
@@ -154,32 +163,40 @@ tsl2561IntegrationTime_t;
 
 typedef enum
 {
-  TSL2561_GAIN_0X                   = 0x00,    // No gain
+  TSL2561_GAIN_1X                   = 0x00,    // No gain
   TSL2561_GAIN_16X                  = 0x10,    // 16x gain
 }
 tsl2561Gain_t;
 
-
-class TSL2561 {
+class Adafruit_TSL2561 : public Adafruit_Sensor {
  public:
-  TSL2561(uint8_t addr);
+  Adafruit_TSL2561(uint8_t addr, int32_t sensorID = -1);
   boolean begin(void);
-  void enable(void);
-  void disable(void);
-  void write8(uint8_t r, uint8_t v);
-  uint16_t read16(uint8_t reg);
-
-  uint32_t calculateLux(uint16_t ch0, uint16_t ch1);
-  void setTiming(tsl2561IntegrationTime_t integration);
+  
+  /* TSL2561 Functions */
+  void enableAutoGain(bool enable);
+  void setIntegrationTime(tsl2561IntegrationTime_t time);
   void setGain(tsl2561Gain_t gain);
-  uint16_t getLuminosity (uint8_t channel);
-  uint32_t getFullLuminosity ();
+  void getLuminosity (uint16_t *broadband, uint16_t *ir);
+  uint32_t calculateLux(uint16_t broadband, uint16_t ir);
+  
+  /* Unified Sensor API Functions */  
+  void getEvent(sensors_event_t*);
+  void getSensor(sensor_t*);
 
  private:
   int8_t _addr;
-  tsl2561IntegrationTime_t _integration;
-  tsl2561Gain_t _gain;
-
-  boolean _initialized;
+  boolean _tsl2561Initialised;
+  boolean _tsl2561AutoGain;
+  tsl2561IntegrationTime_t _tsl2561IntegrationTime;
+  tsl2561Gain_t _tsl2561Gain;
+  int32_t _tsl2561SensorID;
+  
+  void     enable (void);
+  void     disable (void);
+  void     write8 (uint8_t reg, uint32_t value);
+  uint8_t  read8 (uint8_t reg);
+  uint16_t read16 (uint8_t reg);
+  void     getData (uint16_t *broadband, uint16_t *ir);
 };
 #endif
