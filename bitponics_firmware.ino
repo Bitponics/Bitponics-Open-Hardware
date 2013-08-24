03ed627966c78bbae3276ea2d045fb9a21b56033
Michael Zick Doherty <mzdoherty@gmail.com>
2013-08-24 19:52:45 +0800

    updates baud rate

--- bitponics_firmware.ino
+++ bitponics_firmware.ino
@@ -57,7 +57,7 @@ void setup() {
 
   setupRelays();
 
-  Serial.begin(115200);
+  Serial.begin(38400);
   Serial.println();
   Serial.println(F("**********************"));
   Serial.println(F("-> Device Boot"));
