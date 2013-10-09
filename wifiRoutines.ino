dce4bbaf45ea2ce8bd293a4aa7a60d7ef7db88be
Amit Kumar <themit@gmail.com>
2013-10-09 01:45:04 -0400

    updating AP mode network name to be more descriptive

--- wifiRoutines.ino
+++ wifiRoutines.ino
@@ -64,7 +64,7 @@ void wifiAp(){
   //      wifi.setJoin(WIFLY_WLAN_JOIN_ADHOC);
   //      wifi.setDHCP(WIFLY_DHCP_MODE_AUTOIP);
 
-  if(wifi.createAPNetwork("BITPONICS", 1)){
+  if(wifi.createAPNetwork("BitponicsSetup", 1)){
     Serial.println(F("created"));
     Serial.print(F("IP: "));
     Serial.println(wifi.getIP(addr, sizeof(addr)));
